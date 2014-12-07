6c890ec65a6a0b2f8e231093b88c9ee802134a43
Peter Van Bouwel <peter.vanbouwel@ipc.be>
2014-12-07 17:56:07 +0100

    Minor layout optimisation.

--- tests/tests_regionvalidation_nagioscheckcli.py
+++ tests/tests_regionvalidation_nagioscheckcli.py
@@ -19,7 +19,8 @@ class RegionValidationTests(unittest.TestCase):
         cli.process_thresholds = mock_do_nothing
         result = cli.get_validated_region("ALL")
         #Assume that AWS will always have eu-west-1 and us-west-1 and that it will always have more than 5 regions
-        self.assertTrue(isinstance(result,list) and len(result)>=5 and "eu-west-1" in result and "us-west-1" in result)
+        self.assertTrue(isinstance(result, list) and len(result) >= 5 and "eu-west-1" in result
+                        and "us-west-1" in result)
 
     def test_invalid_region_must_raise_value_error(self):
         cli = NagiosCheckCli(["title"])
